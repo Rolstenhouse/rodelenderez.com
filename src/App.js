24225133147eaf2ddb1dc1111fb5946a6ef27786
rolstenhouse <rolsthoorn12@gmail.com>
2021-01-20 18:09:15 -0800

    fix loading of images

--- src/App.js
+++ src/App.js
@@ -25,20 +25,26 @@ const App = () => {
 
   useEffect(() => {
     // Every 2 seconds add another image
-    setInterval(()=>{
-      let x = getRandomInt(10, window.innerWidth-10);
-      let y = getRandomInt(10, window.innerHeight-10);
+    const interval = setInterval(() => {
+      if (!window.innerWidth || !window.innerHeight) { 
+        return
+      }
+      let x = getRandomInt(10, window.innerWidth - 10);
+      let y = getRandomInt(10, window.innerHeight - 10);
       let size = getRandomInt(50, 400);
       const img = {
         url: "/photos/bassoon.jpeg",
         x: x,
         y: y,
         size: size,
-      }
-      setImages([...images, img])
-    }, 2000)
+      };
+      setImages([...images, img]);
+      console.log(images);
+      console.log(img);
+    }, 3000);
 
-  }, []);
+    return () => clearInterval(interval)
+  }, [images]);
 
   return (
     <div className="App">
@@ -49,8 +55,8 @@ const App = () => {
               src={image.url}
               style={{
                 position: "absolute",
-                x: image.x,
-                y: image.y,
+                left: image.x,
+                top: image.y,
                 width: image.size,
               }}
               className="rotate"
